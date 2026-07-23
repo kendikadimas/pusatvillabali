5f0e722a487684d73a3475145855ef9dfabbf4be
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2026-07-24 06:26:17 +0700

    fix: route home render public/home via HomeController instead of welcome

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\OAuthController;
+use App\Http\Controllers\Web\HomeController;
 use App\Http\Controllers\Web\ProfileWebController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
